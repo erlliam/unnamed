71602e5415ef47883c1816601e1bc7315ff17d4a
Erlliam Mejia <tommejas@gmail.com>
2021-10-09 02:08:27 -0400

    Refactor

---
+++ src/dev.js
@@ -0,0 +1,4 @@
+let { createApp } = require("./server");
+
+let app = createApp()
+app.listen(8001);
--- src/server.js
+++ src/server.js
@@ -4,26 +4,29 @@ let formidable = require("formidable");
 let ffprobe = require("ffprobe");
 let fs = require("fs");
 
-let app = express();
-app.use(express.static("public"));
-app.listen(8001);
+function createApp() {
+  let app = express();
+  app.use(express.static("public"));
 
-app.post("/upload", async (req, res, next) => {
-  try {
-    let formData = await parseFormData(req);
-    if (!validExpirationMinutes(formData.fields.expirationMinutes)) {
-      await cleanUpVideoFromFormData(formData);
-      res.status(400).send("Invalid expiration minutes.");
-    } else if (!(await validVideo(formData.files.video))) {
-      await cleanUpVideoFromFormData(formData);
-      res.status(400).send("Invalid video.");
-    } else {
-      res.send("Video is valid. Implement the rest of the crap.");
+  app.post("/upload", async (req, res, next) => {
+    try {
+      let formData = await parseFormData(req);
+      if (!validExpirationMinutes(formData.fields.expirationMinutes)) {
+        await cleanUpVideoFromFormData(formData);
+        res.status(400).send("Invalid expiration minutes.");
+      } else if (!(await validVideo(formData.files.video))) {
+        await cleanUpVideoFromFormData(formData);
+        res.status(400).send("Invalid video.");
+      } else {
+        res.send("Video is valid. Implement the rest of the crap.");
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
-  }
-});
+  });
+
+  return app;
+}
 
 function parseFormData(req) {
   return new Promise((resolve, reject) => {
@@ -74,3 +77,8 @@ async function validVideo(video) {
     return false;
   }
 }
+
+module.exports = {
+  ...module.exports,
+  createApp
+};
