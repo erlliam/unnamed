aecb5c72e662bdae05c7440c728406e109eb3ee3
Erlliam Mejia <tommejas@gmail.com>
2021-10-15 20:24:42 -0400

    Add port to config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 let { createApp } = require("./app.js");
 let { scheduleVideosForDeletion } = require("./deleter.js");
+let { port } = require("../config.json");
 
 let app = createApp();
-app.listen(8001);
+let server = app.listen(port ?? 8001);
 scheduleVideosForDeletion();
