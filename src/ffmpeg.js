fcad1fc4fa3aa6003f627208de35c51a883946b2
Erlliam Mejia <tommejas@gmail.com>
2021-11-04 18:58:46 -0400

    Log some errors

--- src/ffmpeg.js
+++ src/ffmpeg.js
@@ -5,6 +5,10 @@ function ffprobe(path) {
   return new Promise((resolve, reject) => {
     // todo: Crash if ffprobePath is invalid (file doesn't exist or is not ffprobe)
     execFile(ffprobePath, ['-show_streams', '-print_format', 'json', path], (error, stdout, stderr) => {
+      if (error) {
+        console.log('ffprobe error:', error);
+      }
+
       // ffprobe seems to return an empty object regardless if it fails
       resolve(JSON.parse(stdout));
     });
@@ -16,6 +20,10 @@ function convertToMp4(path, newPath) {
   return new Promise((resolve, reject) => {
     // todo: Crash if ffmpegPath is invalid (file doesn't exist or is not ffprobe)
     execFile(ffmpegPath, ['-y', '-i', path, '-c', 'copy', '-f', 'mp4', newPath], (error, stdout, stderr) => {
+      if (error) {
+        console.log('convertToMp4 error:', error);
+      }
+
       if (error === null) {
         resolve(true);
       } else {
